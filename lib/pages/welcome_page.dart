5b36f5dfdfcb54ebffa20aa25623ea137872b523
Yousef Shaban <yousefshaban.eg@gmail.com>
2022-02-19 22:13:37 +0200

    Finish welcome_page (Slider).

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cubit_appp/misc/colors.dart';
+import 'package:flutter_cubit_appp/widgets/app_large_text.dart';
+import 'package:flutter_cubit_appp/widgets/app_text.dart';
+import 'package:flutter_cubit_appp/widgets/responsive_button.dart';
 
 class WelcomePage extends StatefulWidget {
   const WelcomePage({Key? key}) : super(key: key);
@@ -30,11 +34,50 @@ class _WelcomePageState extends State<WelcomePage> {
           child: Container(
             margin: const EdgeInsets.only(top: 150, left: 20, right: 20),
             child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Column(
-                  children: [],
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    AppLargeText(text: "Trips"),
+                    AppText(
+                      text: "Mountain",
+                      size: 30,
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      width: 250,
+                      child: AppText(
+                        text:
+                            "Mountain hikes give you an incredible sense of freedom along with endurance test",
+                        color: AppColors.textColor2,
+                        size: 15,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    ResponsiveButton(width: 120),
+                  ],
                 ),
-                Column(),
+                Column(
+                  children: List.generate(
+                    3,
+                    (indexDots) => Container(
+                      margin: const EdgeInsets.only(bottom: 2),
+                      width: 8,
+                      height: index == indexDots ? 25 : 8,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        color: index == indexDots
+                            ? AppColors.mainColor
+                            : AppColors.mainColor.withOpacity(0.3),
+                      ),
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
